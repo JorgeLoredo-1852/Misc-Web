e9935c7c65a73eed3ce705b9bad05496fa89c1bc
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-07 14:53:26 -0600

    Objects js

--- Module4/script.js
+++ Module4/script.js
@@ -1,3 +1,5 @@
+
+//Basics
 var message = "hi";
 var num = calcular(5,6);
 
@@ -20,3 +22,54 @@ if(x==undefined){
 else{
     console.log("x has been defined");
 }
+
+//Objetos
+var FarmaciaSimilar = {
+    name: "Farmacias Similares",
+    place: "#4221 Fundadores",
+    botarga: {
+        costume: "Policia",
+        employee: "Juan" 
+    }
+};
+
+console.log(FarmaciaSimilar["name"]);
+console.log(FarmaciaSimilar.place);
+console.log(FarmaciaSimilar.botarga["costume"]);
+
+function multiply(x,y){
+    return x*y;
+}
+multiply.version = "v.2";
+console.log(multiply.version);
+
+function makeMultiplier(multiplier){
+   var myFunc = function(x){
+       return multiplier * x;
+   } 
+   return myFunc
+}
+
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+
+function powerOf(power){
+    var newFunc = function(x){
+        var aux = 1;
+        for(i = 0; i<power; i++){
+            aux = aux*x;
+        }
+        return aux;
+    }
+    return newFunc;
+}
+
+var powerOf3 = powerOf(3);
+console.log(powerOf3(7));
+
+function doOperation (x, operation){
+    return operation(x);    
+}
+
+var result = doOperation(7, powerOf3);
+console.log(result);
