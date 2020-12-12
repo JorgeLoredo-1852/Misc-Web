63fb76205f43b253ad6924b55e9d31f5e5b254f4
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-12 13:52:21 -0600

    a little bit of JSX

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,6 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = function () {
+    return (
+        <div className = "container-fluid">
+            <div className = "row">
+                <div className = "col-md">
+                    <div class="card">
+                        <div class="card-horizontal">
+                            <div class="img-square-wrapper">
+                                <img class="" src="logo192.png" alt="Card image cap"/>
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title">Sam Fernandez</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div class= "card-text float-right">Today at 6:00PM</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Button 
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
 function getButtonText(){
     return 'Click on me';
 }
@@ -21,4 +69,4 @@ ReactDOM.render(
     <App/>,
     document.getElementById("root")
 
-);
+);*/
