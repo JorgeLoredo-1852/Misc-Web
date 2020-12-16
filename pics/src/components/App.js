050e722a1f8cec3bb779add21ef4e78ecb277bd5
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-15 18:29:47 -0600

    Developing App

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return(
-        <div className = 'container-fluid' style = {{marginTop: '20px'}}>
-            <div className = 'row'>
-                <div className = "col"></div>
-                <SearchBar/>
-                <div className = "col"></div>
+class App extends React.Component{
+
+    async whenSubmitted(term){
+        const response = await axios.get('https://api.unsplash.com/search/photos',
+        {
+            params:{
+                query:term
+            },
+            headers:{
+                Authorization: 'Client-ID TAzrd2cOEjtdUpYdvDTdiXgbUidwcejwpLP5npr9fPI'
+            }
+        }
+        )
+
+        console.log(response.data.results);
+    }
+
+    render(){
+        return(
+            <div className = 'container-fluid' style = {{marginTop: '20px'}}>
+                <div className = 'row'>
+                    <div className = "col"></div>
+                    <SearchBar onSubmit = {this.whenSubmitted}/>
+                    <div className = "col"></div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
+
+
+//TAzrd2cOEjtdUpYdvDTdiXgbUidwcejwpLP5npr9fPI
+
+//aDviXhQDcGJpX2rsZUAZoUgx_DI4o9yw0ikMJ1frzxQ
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event){
-        console.log(event.target.value);
-    }
+    state = {term:''};
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
 
     render(){
         return(
-            <form className= 'col align-self-center' style ={{border:'1px solid black', borderRadius: '5px'}}>
+            <form onSubmit = {this.onFormSubmit} className= 'col align-self-center' style ={{border:'1px solid black', borderRadius: '5px'}}>
                 <div className="form-group">
                     <label >Image Search</label>
-                    <input onChange = {this.onInputChange} className="form-control"/>
+                    <input 
+                        value = {this.state.term} 
+                        onChange = {(e) => {this.setState({term : e.target.value.toLowerCase()})}} 
+                        className="form-control"
+                    />
                 </div>
             </form>
         );
