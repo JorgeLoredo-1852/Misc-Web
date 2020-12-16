f6211598529fc816b2c614d33089b5c2dd52c6b7
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-16 12:30:03 -0600

    Search Pictures App

--- pics/src/components/ImagesList.js
+++ pics/src/components/ImagesList.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css'
 
 const ImagesList = (props) => {
+
+    
     const imagess = props.images.map( (image) =>{
-        return <img alt = {image.description} key = {image.id} src = {image.urls.regular}/>
+        return <ImageCard key = {image.id} image={image}/>
     }
     );
     
-    return <div>{imagess}</div>
+    return <div className='image-list'>{imagess}</div>
 }
 
 export default ImagesList;
