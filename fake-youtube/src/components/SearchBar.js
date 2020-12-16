359e52e6e0c55854a7f7a1cc43d3557b0f2ecc96
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-16 17:34:59 -0600

    Finished search Bar

---
+++ fake-youtube/src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    state = {term: ''};
+    
+    onInputChange = (event) =>{
+        this.setState({term : event.target.value});
+    }
+
+    onFormSubmit = (event) =>{
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
+    render(){
+        return(
+        <div className = 'row' style = {{margin: '20px'}}>
+            <form className="form-floating col-12" onSubmit = {this.onFormSubmit}>
+                <label for="exampleFormControlInput1" className="form-label-lg">Video Search</label>
+                <input 
+                    onChange = {this.onInputChange} 
+                    className="form-control form-control-lg" 
+                    type="text"/>
+            </form>
+        </div>   
+        );
+    }
+}
+
+export default SearchBar;
