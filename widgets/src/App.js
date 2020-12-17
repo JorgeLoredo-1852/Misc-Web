0c462669ad0a899665aafff2c262c7e074b66199
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-17 15:27:34 -0600

    Setup

---
+++ widgets/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'Porque el Dr Simi?',
+        content: 'El doctor Simi es símbolo nacional, aparte de guapo y tener un buen tamaño de posaderas'
+    },
+    {
+        title: 'Dr Simi o Vaquita Alpura',
+        content: 'Claramente un debate formidable, pero los pasos del dr simi resultan más felxibles en cuanto a género de música'
+    },
+    {
+        title: 'Quien eres',
+        content: 'Jorge Loredo mucho gusto por encontrarme contigo, en el underground también me conocen como JG'
+    }
+];
+
+
+const App = () =>{
+    return(
+        <div>
+            <Accordion items = {items}/>
+        </div>
+    );
+}
+
+export default App;
