21c0b5fc78ff430813c5360629326481070d373f
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-17 13:46:43 -0600

    Project Complete

--- fake-youtube/src/components/App.js
+++ fake-youtube/src/components/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/YouTubeApi';
 import VideosList from './VideosList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
 
     state = {videos : [], selectedVideo : null};
 
+    componentDidMount(){
+        this.onSearchTerm('Rapder');
+    }
+
     onSearchTerm = async (term) => {
         const list = await youtube.get('/search',
             {params : {q : term}}
         );
-        this.setState({videos : list.data.items});
+        this.setState({videos : list.data.items, selectedVideo:list.data.items[0]});
     } 
     
     onVideoSelect = (video) => {
-        console.log(video);
+        this.setState({selectedVideo: video});
     }
 
     render(){
@@ -24,11 +29,12 @@ class App extends React.Component{
                 <div className = 'container'>
                     <SearchBar onSearchTerm = {this.onSearchTerm}/>   
                     <div className = 'row' style={{margin: '20px'}}>
-                        <div className = 'col-7'></div>
+                        <div className = 'col-7'>
+                            <VideoDetail video = {this.state.selectedVideo}/>
+                        </div>
                         <VideosList onVideoSelect = {this.onVideoSelect} videos = {this.state.videos}/>
                     </div>
                 </div>
-
             </div>
         );
     }
--- fake-youtube/src/components/VideoItem.js
+++ fake-youtube/src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = (props) =>{
     return(
         <a href="#" onClick={() => props.onVideoSelect(props.video)} class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-between video-item">
-                <img src = {props.video.snippet.thumbnails.medium.url} style={{maxWidth:'120px',maxHeight:'80px'}}/>
+                <img alt = "" src = {props.video.snippet.thumbnails.medium.url} style={{maxWidth:'120px',maxHeight:'80px'}}/>
                 <div id="titulo">{props.video.snippet.title}</div>
             </div>
         </a>
