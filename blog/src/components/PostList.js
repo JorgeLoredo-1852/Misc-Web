3e5251dd701198d59b9cd0ebef58eddebff603d7
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-21 14:41:54 -0600

    Blog setup

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostLists extends React.Component{
+    render(){
+        return (
+            <div>
+                PostLists
+            </div>
+        );
+    }
+}
+
+export default PostLists;
