44d492916ca2362d4cc654b4f5c0f86f1e2ded07
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-23 10:41:43 -1000

    Route Example

--- streaming_app/client/src/components/App.js
+++ streaming_app/client/src/components/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const pageOne = () =>{
+    return (
+    <div>
+        PageOne
+        <Link to="/page2">Page 2</Link>
+    </div>);
+};
+
+const pageTwo = () =>{
+    return (
+    <div>
+        pageTwo
+        <Link to="/">Page1</Link>
+    </div>);
+};
 
 const App = () => {
     return(
         <div>
-            App
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={pageOne}/>
+                    <Route path="/page2" exact component={pageTwo}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
