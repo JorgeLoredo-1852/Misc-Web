f123b90aadd3d57cfe46d115d33b0a3ba2601fcb
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2020-12-27 14:04:27 -0600

    Google Auth complete

--- streaming_app/client/src/components/GoogleAuth.js
+++ streaming_app/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component{
 
@@ -7,17 +9,54 @@ class GoogleAuth extends React.Component{
             window.gapi.client.init({
                 clientId:"743190205416-ruuddvg77bg5lcmnbvng4t0q7tmkj6bu.apps.googleusercontent.com",
                 scope:"email"
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.authOnChange(this.auth.isSignedIn.get());
+                this.auth.isSignedIn.listen(this.authOnChange);
             })
         })
+    }
+
+    authOnChange = (isSignedIn) => {
+        if(isSignedIn){
+            this.props.signIn(this.auth.currentUser.get().getId());
+        } else {
+            this.props.signOut();
+        }
+    };
+
+    renderAuth() {
+        if(this.props.isSignedIn === null){
+            return <div></div>;
+        } else if(this.props.isSignedIn){
+            return (
+            <div>
+                <button onClick={this.auth.signOut} type="button" className="btn btn-info">
+                    <i className="bi bi-google"></i>
+                    Sign Out
+                </button>
+            </div>);
+        } else {
+            return (<div>
+                <button onClick={this.auth.signIn} type="button" className="btn btn-info">
+                    <i className="bi bi-google"></i>
+                    Sign In
+                </button>
+            </div>);
+        }
     }
 
     render(){
         return(
             <div>
-                GoogleAuth
+                {this.renderAuth()}
             </div>
         );
     }
 };
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+    return ({isSignedIn : state.auth.isSignedIn});
+};
+
+export default connect(mapStateToProps,{signIn,signOut})(GoogleAuth);
--- streaming_app/client/src/components/Header.js
+++ streaming_app/client/src/components/Header.js
@@ -5,18 +5,18 @@ import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
     return(
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <Link class="navbar-brand" to="/">Streamy</Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">Streamy</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse " id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <Link class="nav-link" to="/">All Streams</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">All Streams</Link>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link ">
+                <li className="nav-item">
+                    <a className="nav-link ">
                         <GoogleAuth/>
                     </a>
                 </li>
