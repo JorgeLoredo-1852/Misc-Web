14795d0b373853a565ab8cafa3bf6abcda04abcf
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2021-01-03 13:20:05 -0600

    language context setup

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component{
+    state = {language: "english"};
+
+    onLanguageChange = language => {
+        this.setState({language});
+    }
+
     render(){
         return(
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <div>
+                    Select a Language: 
+                    <i className="flag us" onClick={()=>this.onLanguageChange("english")}></i>
+                    <i className="flag nl" onClick={()=>this.onLanguageChange("dutch")}></i>
+                </div>
+                <UserCreate/>
+            </div>
         );
     }
 }
