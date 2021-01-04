d2c0cd6a2574bb8194a3938953ad918a560b5939
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2021-01-04 17:23:09 -0600

    Created fan page

--- Css/simipage/src/components/Header/Header.js
+++ Css/simipage/src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Header.css';
+import logo from './logo-simi.png';
 
 const Header = () =>{
     return (
         <div className="header">
-            
+            <div className="logo-box">
+                <img src={logo} alt="logo" className="logo-image"></img>
+            </div>
+            <div className="text-box">
+                <h1 className="text-primary">
+                    <span className="text-primary-main">Dr simi</span>
+                    <span className="text-primary-sub">Club de fans oficial</span>
+                </h1>
+            </div>
         </div>
     );
 };
