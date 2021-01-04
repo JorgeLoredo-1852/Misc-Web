c22abc39d528dfc55c16d2efeb0bc9e64f3ca8eb
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2021-01-04 14:16:21 -0600

    Started on Css

--- Css/simipage/src/components/App.js
+++ Css/simipage/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './style.css';
+import Header from './Header/Header';
 
 const App = () =>{
     return(
-        <div>
-            App
-        </div>
+        <React.Fragment>
+            <Header/>
+        </React.Fragment>
     );
 };
 
