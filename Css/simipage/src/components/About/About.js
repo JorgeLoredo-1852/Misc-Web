2cb82e9bae018c7a16c5df4ce264d7dfa1559eba
JorgeLoredo-1852 <jorgeloremele@gmail.com>
2021-01-07 13:46:17 -0600

    About section created

---
+++ Css/simipage/src/components/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './_About.css';
+
+const About = () => {
+    return(
+        <main>
+            <section className="about-section">
+                <div className= "u-center-content u-margin-bottom-8">
+                    <h2 className="about-section-heading">Tú doctor de cabeza </h2>
+                    <div className="row">
+                        <div className="col-1-of-2">Text content</div>
+                        <div className="col-1-of-2">Text Images</div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+};
+
+export default About;
--- Css/simipage/src/components/App.js
+++ Css/simipage/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import Header from './Header/Header';
+import About from './About/About';
 
 const App = () =>{
     return(
         <React.Fragment>
             <Header/>
+            <About/>
         </React.Fragment>
     );
 };
